ac1810c05e57dd6745b7002dc5682fc03db00200
Martin <martinmwai901@gmail.com>
2025-11-03 09:46:35 +0300

    hey

--- components/SpotifyNowPlaying.tsx
+++ components/SpotifyNowPlaying.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import { MusicNote03Icon, SpotifyIcon } from 'hugeicons-react';
+import { SpotifyIcon } from 'hugeicons-react';
 import Image from 'next/image';
 
 interface SpotifyTrack {
